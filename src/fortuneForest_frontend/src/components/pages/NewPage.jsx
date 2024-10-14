03e565847bf934a7448acb574fe1ca9e82f8bdec
Azer <azersagucio@gmail.com>
2024-10-14 20:01:53 +0800

    slotmachinecomponent

---
+++ src/fortuneForest_frontend/src/components/pages/NewPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyComponent from '../MyComponent';
+
+const NewPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the New Page</h1>
+      <MyComponent />
+    </div>
+  );
+};
+
+export default NewPage;
---
+++ src/fortuneForest_frontend/src/components/pages/slotmachine.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MachineComponent from '../slotmachinecomponent';
+
+const slotmachine = () => {
+  return (
+    <div>
+      <h1>Slot Machine</h1>
+      <MachineComponent />
+    </div>
+  );
+};
+
+export default slotmachine;
